121d9b5219be12e0523b232b73ee294d44f72c82
Jacob Yeung <xohkgolden-git@yahoo.com>
2023-10-28 15:25:08 -0700

    completed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double square_root(double x, double a, double tolerance =  0.0001);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x;
+    std::cin >> x;
+
+    cout << setprecision(2) << fixed << square_root(x, x) << endl;
+
     return 0;
+}
+
+// https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+double square_root(double x, double a, double tolerance) {
+    return (abs( a * a - x) <= tolerance) ? a :
+    square_root(x, (a * a + x) / (2 * a));
 }
